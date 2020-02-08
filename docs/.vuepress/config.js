5add5abfce84a6af635144c337184b2ee5d5688f
asen <huisen0705@live.com>
2020-02-08 10:32:11 +0800

    ,,,update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -14,18 +14,17 @@ module.exports = {
     themeConfig: {
         nav: [
           { text: '主页', link: '/' },
-          { text: '前端规范', link: '/frontEnd/' },
-          { text: '开发环境', link: '/development/' },
-          { text: '学习文档', link: '/notes/' },
-          { text: '游魂博客', link: 'https://www.iyouhun.com' },
+          { text: 'C#', link: '/csharp/' },
+          { text: 'Sql Server', link: '/sqlserver/' },
+          { text: '随笔', link: '/essay/' },
           // 下拉列表的配置
-        //   {
-        //     text: 'Languages',
-        //     items: [
-        //       { text: 'Chinese', link: '/language/chinese' },
-        //       { text: 'English', link: '/language/English' }
-        //     ]
-        //   }
+            //   {
+            //     text: 'Languages',
+            //     items: [
+            //       { text: 'Chinese', link: '/language/chinese' },
+            //       { text: 'English', link: '/language/English' }
+            //     ]
+            //   }
         ]
     }
 }
